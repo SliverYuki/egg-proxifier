da34e920cb4c02d77220877e6f45216cf12ec2a3
李颜余 <liyanyu@23mofang.com>
2019-11-21 18:48:02 +0800

    chore: init commit

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'max-len': 0,
+    'no-bitwise': 0,
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'no-restricted-globals': 0,
+    'class-methods-use-this': 0,
+    'consistent-return': 0,
+    'no-use-before-define': 0,
+    'prefer-destructuring': 0,
+    'global-require': 0,
+
+    'indent': [2, 2, { SwitchCase: 1 }],
+    'linebreak-style': [2, 'unix'],
+    'no-trailing-spaces': 2,
+    'eol-last': [2, 'always'],
+    'no-multiple-empty-lines': [2, { max: 1 }],
+    'no-unused-expressions': [2, { 'allowShortCircuit': true, 'allowTernary': true }],
+    'object-curly-newline': [2, { 'multiline': true }],
+    'no-plusplus': [2, { 'allowForLoopAfterthoughts': true }],
+    'radix': [2, 'as-needed'],
+    'quote-props': [2, 'as-needed', { 'numbers': true }],
+    'semi': [2, 'never'],
+    'camelcase': [2, { properties: 'never' }],
+    'comma-dangle': [2, 'always-multiline'],
+    'arrow-parens': [2, 'as-needed'],
+    'space-before-function-paren': [
+      2,
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+  },
+  globals: {}
+}
---
+++ index.js
@@ -0,0 +1,76 @@
+const httpProxy = require('http-proxy')
+const pathMatch = require('path-match')
+
+const proxy = httpProxy.createProxyServer()
+const route = pathMatch({
+  sensitive: false,
+  strict: false,
+  end: false,
+})
+
+let eventRegistered = false
+
+/**
+ * @param {string} path - 匹配路径
+ * @param {Object} options - 配置项，参考 https://github.com/http-party/node-http-proxy/blob/master/README.md#options
+ * @param {function} options.rewrite - 参数为 path，用于对 path 进行操作
+ * @param {Object}  options.events - proxy 事件，key 值为事件名,参考 https://github.com/http-party/node-http-proxy/blob/master/README.md#listening-for-proxy-events
+ */
+module.exports = (path, options) => (ctx, next) => {
+  const match = route(path)
+  if (!match(ctx.path)) return next()
+
+  let opts = { ...options }
+  if (typeof options === 'function') {
+    const params = match(ctx.path)
+    opts = options.call(options, params)
+  }
+
+  const { rewrite, events } = opts
+
+  const httpProxyOpts = Object.keys(opts)
+    .filter(n => ['rewrite', 'events'].indexOf(n) < 0)
+    .reduce((prev, cur) => {
+      prev[cur] = opts[cur]
+      return prev
+    }, {})
+
+  return new Promise((resolve, reject) => {
+    ctx.req.oldPath = ctx.req.url
+
+    if (typeof rewrite === 'function') {
+      ctx.req.url = rewrite(ctx.req.url, ctx)
+    }
+
+    if (events && typeof events === 'object' && !eventRegistered) {
+      Object.entries(events).forEach(([event, handler]) => {
+        proxy.on(event, handler)
+      })
+      eventRegistered = true
+    }
+
+    /**
+     * 这里有一个 BUG (或许是文档错误) callback 实际上应该是 errorHandler
+     * 在这里做了一点处理以让 promise 在 proxy.web 之后能正确的 resolve
+     * https://github.com/nodejitsu/node-http-proxy/issues/951#issuecomment-179904134
+     */
+    ctx.res.on('close', () => {
+      reject(new Error(`Http response closed while proxying ${ctx.req.oldPath}`))
+    })
+
+    ctx.res.on('finish', () => {
+      resolve()
+    })
+
+    proxy.web(ctx.req, ctx.res, httpProxyOpts, e => {
+      const status = {
+        ECONNREFUSED: 503,
+        ETIMEOUT: 504,
+      }[e.code]
+      ctx.status = status || 500
+      resolve()
+    })
+  })
+}
+
+module.exports.proxy = proxy
